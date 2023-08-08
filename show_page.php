28ec2538fbfb9f324618ae883c47286511ed3251
Nicollette Porca <20200108m.porca.nicollette.bscs@gmail.com>
2023-08-08 11:55:44 +0800

    porca branch

---
+++ show_page.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UCC REAP</title>
+    <link rel="icon" href="dist/image/UCC.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="dist/css/links_page.css">
+</head>
+
+<body class="bg-light">
+
+    <header style="background: rgb(53, 144, 53);">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand fw-semibold" href="#">
+                    <img src="dist/image/UCC.png" alt="UCC Logo" width="50" height="55" class="me-2">
+                    UCC ReaP
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <div class="input-group">
+                                <input class="form-control me-1 rounded-5" type="search" placeholder="Search..."
+                                    aria-label="Search" id="search">
+                                <span class="input-group-append">
+                                    <button class="btn bg-none rounded-5 text-white" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <button class="btn text-white me-1" type="submit">Join now</button>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="btn btn-outline-light rounded-5">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="mt-4 mb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="title">
+                        <label class="fw-semibold h2">UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH</label>
+                    </div>
+                    <div class="date">
+                        <label class="fst-italic">August 19, 2002</label>
+                    </div>
+                    <div class="researchers mb-4">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">&bull; Andre Mandantes</li>
+                            <li class="list-inline-item">&bull; Angelo Perlota</li>
+                            <li class="list-inline-item">&bull; Cedric Cruz</li>
+                            <li class="list-inline-item">&bull; Kenjie Arceo</li>
+                            <li class="list-inline-item">&bull; Nicollette Porca</li>
+                        </ul>
+                    </div>
+                    <div class="card p-4 rounded-0 mb-4">
+                        <div class="abstract-text">
+                            <label class="fw-semibold fs-5 mb-3">ABSTRACT</label>
+                        </div>
+                        <div class="abstract-body">
+                            <label>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit
+                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </label>
+                        </div>
+                    </div>
+                    <div class="citation text-muted">
+                        <label>Citation:</label> <br>
+                        <ul class="list-inline">
+                            <li class="list-inline-item" id="authors">Arceo, K., </li>
+                            <li class="list-inline-item" id="year">(2023), </li>
+                            <li class="list-inline-item" id="title">"University of Caloocan City Title Research", </li>
+                            <li class="list-inline-item fst-italic fw-bold" id="journal-title">
+                                The New Crossroads, 
+                            </li>
+                            <li class="list-inline-item" id="pages">pp. 6969.</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <hr class="container text-muted">
+
+                <div class="col-sm-4">
+                    <div class="related-studies">
+                        <label class="text-muted fs-4 mb-3">RELATED STUDIES</label>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card p-1 rounded-0 mb-4">
+                        <div class="card p-3 border-0">
+                            <a href="#" class="research-title fw-semibold fs-5">
+                                UNIVERSITY OF CALOOCAN CITY TITLE RESEARCH
+                            </a>
+                            <a href="#" class="date fst-italic text-muted">
+                                August 19, 2002
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-white text-center text-muted border">
+        <div class="text-center p-3">
+            <strong>Copyright &copy; 2023-2024. <a href="https://www.ucc-caloocan.edu.ph/"
+                    class="link text-muted">University of Caloocan City</a>.
+            </strong> All rights reserved.
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
